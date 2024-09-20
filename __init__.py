b1a15a5f1608c6b8b3704566297ada54257a3c11
Daniel McKnight <daniel@neon.ai>
2024-09-20 09:48:34 -0700

    Update OCP imports to ovos-utils Update ovos-utils dependency to 0.1 for import compat

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ import requests
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 from neon_utils.file_utils import load_commented_file
-from ovos_plugin_common_play import MediaType, PlaybackType
+from ovos_utils.ocp import MediaType, PlaybackType
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     ocp_search
 from ovos_utils import classproperty
